d33dbce72930ed98329f302ad261948550b704e8
krlex <krle@tilda.center>
2018-11-10 08:26:48 +0100

    change to debian stretch and new packages

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ PATH="${HOME}/.local/bin:${PATH}"
 virtual_env="provision"
 virtual_env_path=~/.virtualenvs/$virtual_env
 
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev
-apt-get install -y sudo python-pip python-dev sshpass
-pip install --user vex
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
+apt-get install -y sudo python3-pip python3-dev sshpass
+pip3 install --user vex
 vex --make $virtual_env pip install -U pip
 source $virtual_env_path/bin/activate
-pip install -U ansible
+pip3 install -U ansible
 ansible-playbook -i provision/inventory/localhost provision/site.yml -c local
 
 rm -rf ~/.virtualenvs
