2dc23cd7b6a4d0aca932c8a73de0030e77350d53
krlex <krle@tilda.center>
2019-08-14 07:27:51 +0200

    config installation docker

--- run.sh
+++ run.sh
@@ -5,10 +5,9 @@ PATH="${HOME}/.local/bin:${PATH}"
 virtual_env="provision"
 virtual_env_path=~/.virtualenvs/$virtual_env
 
-sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
-apt-get install -y sudo python3-pip python3-dev sshpass
+sudo apt-get install -y build-essential libssl-dev libffi-dev python3-dev sudo python3-pip sshpass
 pip3 install --user vex
-vex --make $virtual_env pip install -U pip
+vex --make $virtual_env pip3 install -U pip
 source $virtual_env_path/bin/activate
 pip3 install -U ansible
 ansible-playbook -i provision/inventory/localhost provision/site.yml -c local
